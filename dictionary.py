925e7e0a42e9ff1d2b44a0692bc71f004a5a6d81
Douglas Mellon <douglas12mellon@gmail.com>
2020-02-19 23:30:47 -0700

    Dictionary Tests
    Used both an O(n) and O(1) method from the book. The algorithm can easily be used with each dictionary action by replacing a single line. I am getting tiny numbers in return for the time so my computer processes are actually messing with them. Will have to check on the computers in the lab.

--- dictionary.py
+++ dictionary.py
@@ -19,16 +19,33 @@ def dictionary_tests(type, max_size, tries):
 
         for i in range(tries):
             #  create an initial dictionary of random keys and size.
-            initial_dictionary = {str(i): "value" for i in range(random.randint(0, max_size))}
+            initial_dictionary = {str(i): "value" for i in range(random.randint(1, max_size))}
 
             start_time = time.time() # time of start
             copied_dictionary = initial_dictionary.copy()
             end_time = time.time() # time of completion
-            times_list.append(round(end_time - start_time, 6))
+            times_list.append(end_time - start_time)
+            size_list.append(len(initial_dictionary))
+
+        append_to_csv(times_list, size_list)
+        return times_list, size_list
+
+    if type.lower() == 'get item':
+
+        for i in range(tries):
+            #  create an initial dictionary of random keys and size.
+            initial_dictionary = {str(i): "value" for i in range(random.randint(1, max_size))}
+
+            random_index = random.randint(0, len(initial_dictionary)) #  generate a random index to pull from the dictionary
+
+            start_time = time.time()  # time of start
+            initial_dictionary.get(random_index) # get index from dictionary
+            end_time = time.time()  # time of completion
+            times_list.append(end_time - start_time)
             size_list.append(len(initial_dictionary))
 
         append_to_csv(times_list, size_list)
         return times_list, size_list
 
 
-print(dictionary_tests('copy', 10, 10))
+print(dictionary_tests('get item', 10, 10))
