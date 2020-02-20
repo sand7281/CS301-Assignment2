84366ddbb8dcd643c8f0200b5673ba93f2de75cd
sand7281 <55165536+sand7281@users.noreply.github.com>
2020-02-20 08:44:19 -0700

    New

---
+++ Dictionary!!.py
@@ -0,0 +1,48 @@
+import time
+import random
+from csv import writer
+
+
+def append_to_csv(times_list, size_list):
+    with open('assignment2.csv', 'a+', newline='') as write_obj:
+        csv_writer = writer(write_obj)
+        csv_writer.writerow(size_list)
+        csv_writer.writerow(times_list)
+
+
+def dictionary_tests(type, max_size, tries):
+
+    times_list = [] # list of times
+    size_list = [] # list of sizes
+
+    if type.lower() == 'copy':
+
+        for i in range(tries):
+            #  create an initial dictionary of random keys and size.
+            initial_dictionary = {str(i): "value" for i in range(random.randint(1, max_size))}
+
+            start_time = time.time() # time of start
+            copied_dictionary = initial_dictionary.copy()
+            end_time = time.time() # time of completion
+            times_list.append(end_time - start_time)
+            size_list.append(len(copied_dictionary))
+
+        append_to_csv(times_list, size_list)
+        return times_list, size_list
+
+    if type.lower() == 'get':
+
+        for i in range(tries):
+            #  create an initial dictionary of random keys and size.
+            initial_dictionary = {str(i): "value" for i in range(random.randint(1, max_size))}
+		
+            start_time = time.time() # time of start
+            initial_dictionary.get(random.randint(0, len(initial_dictionary)))
+            end_time = time.time() # time of completion
+            times_list.append(end_time - start_time)
+            size_list.append(len(initial_dictionary))
+        append_to_csv(times_list, size_list)
+        return times_list, size_list
+
+
+print(dictionary_tests('get', 1000000, 10))
